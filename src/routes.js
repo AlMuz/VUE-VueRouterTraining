e0c7b53308bf207d38044dce1d1e5fe2edc1491f
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-07-18 16:35:35 +0300

    Redirecting example

--- src/routes.js
+++ src/routes.js
@@ -19,4 +19,5 @@ export const routes = [
     {path: ':id', component: UserDetail },
     {path: ':id/edit', component: UserEdit, name: 'userEdit' },
   ]},
+  { path: '/redirect-me', redirect: '/user'}
 ];
